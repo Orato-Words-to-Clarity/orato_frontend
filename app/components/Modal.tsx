89d36f9a5a7fef0b52c477f18a0d2665ecacc500
edwineas <edwinalexshaji@gmail.com>
2025-03-22 18:28:48 +0530

    refactor(modal):change-props

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
-import { ModalProps } from '@/app/types';
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, children, zIndexCount, style }) => {
+// const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, children, zIndexCount, style }) => {
+const Modal = ({
+  isOpen,
+  onClose,
+  title,
+  zIndexCount,
+  children,
+  style,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  children: React.ReactNode;
+  zIndexCount?: number;
+  style?: React.CSSProperties;
+}) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className='sm:max-w-[425px] w-[95%]' style={{ zIndex: zIndexCount, ...style }}>
--- app/types.ts
+++ app/types.ts
@@ -4,12 +4,3 @@ export type CredentialDataType = {
   email: string;
   password: string;
 };
-
-export type ModalProps = {
-  children: React.ReactNode;
-  style?: React.CSSProperties;
-  isOpen: boolean;
-  onClose?: () => void;
-  title?: string;
-  zIndexCount?: number;
-};
